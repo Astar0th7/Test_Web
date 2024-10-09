b2a381059c7d5e07535c03095e25f065ae68fe70
Astar0th <astar0th-9@yandex.ru>
2024-10-09 16:53:50 +0700

    Added service locator for resolve dependency

--- Assets/Game/Scripts/Button.cs
+++ Assets/Game/Scripts/Button.cs
@@ -3,35 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Button : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler, IPointerEnterHandler {
-    bool on;
-    Animator animator;
-    public void OnPointerClick(PointerEventData eventData) {
+public class Button : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private Animator _animator;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
         Debug.Log("Click");
-        on = true;
-    }
-
-    public void OnPointerDown(PointerEventData eventData) {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnPointerEnter(PointerEventData eventData) {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnPointerExit(PointerEventData eventData) {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnPointerUp(PointerEventData eventData) {
-        throw new System.NotImplementedException();
-    }
-
-    void Update() {
-        if (on) {
-            animator = GetComponent<Animator>();
-            animator.SetBool("anim", true);
-            GameObject.Find("Panel").GetComponent<LoadSprites>().load();
-        }
+        
+        _animator = GetComponent<Animator>();
+        _animator.SetBool("anim", true);
+        GameObject.Find("Panel").GetComponent<LoadSprites>().load();
     }
 }
---
+++ Assets/Game/Scripts/Root/Services/ServiceLocator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game.Scripts.Root.Services
+{
+    public class ServiceLocator
+    {
+        public static ServiceLocator _instance;
+
+        public static ServiceLocator Instance => _instance ??= new ServiceLocator();
+
+        private Dictionary<Type, object> _services = new();
+
+        public void RegisterSingle<T>(T service)
+        {
+            Type type = typeof(T);
+            if (_services.TryAdd(type, service) == false)
+                throw new ArgumentException("Service already registered");
+        }
+
+        public T Resolve<T>()
+        {
+            Type type = typeof(T);
+            if (_services.TryGetValue(type, out object service) == false)
+                throw new ArgumentException("Service not registered");
+            
+            return (T) service;
+        }
+    }
+}
